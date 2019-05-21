984e77a1633d7f591bd30ff5a2d077dfb9bc6f59
恩翊 <enyi.lr@cainiao.com>
2019-05-22 00:08:34 +0800

    some solutions

---
+++ src/main/java/leetcode/Solution215.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author enyi.lr
+ * @version $Id: Solution215.java, v 0.1 2019‐05‐21 12:41 AM enyi.lr Exp $$
+ */
+public class Solution215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        Collections.sort(list);
+        Integer integer = list.get(nums.length - k);
+        return integer;
+    }
+
+}
