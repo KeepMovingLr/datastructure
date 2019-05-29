125c9ddb6981b7819db3ead79297dc2513e57f70
恩翊 <enyi.lr@cainiao.com>
2019-05-29 19:39:36 +0800

    intersection of two arrays

---
+++ src/main/java/leetcode/Solution349.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * @author enyi.lr
+ * @version $Id: Solution349.java, v 0.1 2019‐05‐23 2:02 AM enyi.lr Exp $$
+ */
+public class Solution349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set1.contains(nums2[i])) {
+                set2.add(nums2[i]);
+            }
+        }
+        int[] result = new int[set2.size()];
+        Object[] objects = set2.toArray();
+        for (int i = 0; i < objects.length; i++) {
+            result[i] = (Integer) objects[i];
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/leetcode/Solution350.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author enyi.lr
+ * @version $Id: Solution350.java, v 0.1 2019‐05‐23 2:02 AM enyi.lr Exp $$
+ */
+public class Solution350 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            Integer orDefault = map1.getOrDefault(nums1[i], 0);
+            orDefault++;
+            map1.put(nums1[i], orDefault);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (map1.containsKey(nums2[i])) {
+                Integer count = map1.get(nums2[i]);
+                if (count > 0) {
+                    Integer orDefault = resultMap.getOrDefault(nums2[i], 0);
+                    orDefault++;
+                    resultMap.put(nums2[i], orDefault);
+                    count--;
+                    map1.put(nums2[i], count);
+                }
+            }
+        }
+        int total = 0;
+        for (Integer key : resultMap.keySet()) {
+            total = total + resultMap.get(key);
+        }
+        int[] result = new int[total];
+        int j = 0;
+        for (Integer key : resultMap.keySet()) {
+            for (int i = 0; i < resultMap.get(key); i++) {
+                result[j] = key;
+                j++;
+            }
+
+        }
+        return result;
+    }
+
+}
