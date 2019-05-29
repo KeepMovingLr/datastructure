ed67c4e9ae7371fb61ca1a57bbaf887a8190f3d7
恩翊 <enyi.lr@cainiao.com>
2019-05-29 19:12:57 +0800

    709

---
+++ src/main/java/leetcode/Solution709.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * @author enyi.lr
+ * @version $Id: Solution709.java, v 0.1 2019‐05‐24 12:38 AM enyi.lr Exp $$
+ */
+public class Solution709 {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
