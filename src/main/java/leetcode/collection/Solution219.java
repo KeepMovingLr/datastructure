c1b657822d742e14d055c9fd689780384b5993f9
恩翊 <enyi.lr@cainiao.com>
2019-06-07 15:15:30 +0800

    leetcode solution 219

---
+++ src/main/java/leetcode/collection/Solution219.java
@@ -0,0 +1,27 @@
+package leetcode.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author enyi.lr  how to deal with problem using set and map
+ * @version $Id: Solution15.java, v 0.1 2019‐05‐28 7:33 PM enyi.lr Exp $$
+ */
+public class Solution219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
