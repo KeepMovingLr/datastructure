44b515262dcaeffefd65d9dfe85998dd39301638
恩翊 <enyi.lr@cainiao.com>
2019-06-09 12:54:03 +0800

    priority queue

---
+++ src/main/java/queue/PriorityQueue.java
@@ -0,0 +1,41 @@
+package queue;
+
+import maxheap.MaxHeap;
+
+/**
+ * @author enyi.lr
+ * @version $Id: PriorityQueue.java, v 0.1 2019‐05‐25 3:38 PM enyi.lr Exp $$
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
