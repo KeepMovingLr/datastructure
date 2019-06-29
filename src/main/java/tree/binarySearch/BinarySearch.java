489376601013ba67411524b14b4f9f168990e809
恩翊 <enyi.lr@cainiao.com>
2019-06-29 11:12:23 +0800

    binary search

---
+++ src/main/java/tree/binarySearch/BinarySearch.java
@@ -0,0 +1,79 @@
+package tree.binarySearch;
+
+/**
+ * @author enyi.lr
+ * @version $Id: BinarySearch.java, v 0.1 2019‐06‐24 10:06 PM enyi.lr Exp $$
+ */
+public class BinarySearch {
+
+    /**
+     * Returns index of x if it is present in array, else return -1
+     */
+    public int binarySearch(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        int mid = l + (r - l) / 2;
+        while (l <= r) {
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                l = mid + 1;
+                mid = l + (r - l) / 2;
+            } else {
+                r = mid - 1;
+                mid = l + (r - l) / 2;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Returns index of target if it is present in arr[l,r], else return -1
+     *
+     * @param arr
+     * @param l
+     * @param r
+     * @param target
+     * @return
+     */
+    public int binarySearch(int[] arr, int l, int r, int target) {
+        int mid = l + (r - l) / 2;
+        if (l <= r) {
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                return binarySearch(arr, mid + 1, r, target);
+            } else {
+                return binarySearch(arr, l, mid - 1, target);
+            }
+        } else {
+            return -1;
+        }
+
+    }
+
+    /**
+     * get the index with the largest data smaller or equal to target
+     * there are two ways to solve the problem. one is for loop the whole array.
+     * @param arr a sorted array
+     * @param target
+     * @return
+     * referer https://www.geeksforgeeks.org/floor-in-a-sorted-array/
+     */
+    public int floor(int[] arr, int target) {
+
+        return -1;
+
+    }
+
+    /**
+     * get the index with the smallest data larger than or equal to target
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public int ceil(int[] arr, int target) {
+        return -1;
+    }
+}
