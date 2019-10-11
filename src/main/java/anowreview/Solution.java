83b498d8bb9954d3462c317518b33d5f655c7a35
恩翊 <enyi.lr@cainiao.com>
2019-10-11 20:28:24 +0800

    new review Solution

---
+++ src/main/java/anowreview/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package anowreview;
+
+
+/**
+ * @author enyi.lr
+ * @version $Id: Solution.java, v 0.1 2019‐10‐11 8:00 PM enyi.lr Exp $$
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
