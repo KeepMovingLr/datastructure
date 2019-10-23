c74512040960956085c6d15a07e4d50290de0028
恩翊 <enyi.lr@cainiao.com>
2019-10-23 13:29:37 +0800

    solution349 another method

--- src/main/java/leetcode/collection/usesetsolve/Solution349.java
+++ src/main/java/leetcode/collection/usesetsolve/Solution349.java
@@ -1,10 +1,15 @@
 package leetcode.collection.usesetsolve;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author enyi.lr
  * @version $Id: Solution349.java, v 0.1 2019‐05‐23 2:02 AM enyi.lr Exp $$
+ * v2 2019‐10‐23
  */
 public class Solution349 {
     public int[] intersection(int[] nums1, int[] nums2) {
@@ -25,5 +30,29 @@ public class Solution349 {
         }
         return result;
     }
+
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new TreeSet<>();
+        Set<Integer> set2 = new TreeSet<>();
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        for (int i : nums2) {
+            set2.add(i);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Integer integer : set1) {
+            if (set2.contains(integer)){
+                list.add(integer);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+
 
 }
