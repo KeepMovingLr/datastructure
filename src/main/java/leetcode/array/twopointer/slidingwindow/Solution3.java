8b8864c9ac846ee42afd2a7931c693fed9fb0cdb
恩翊 <enyi.lr@cainiao.com>
2019-10-29 22:20:41 +0800

    sliding window

--- src/main/java/leetcode/array/twopointer/slidingwindow/Solution3.java
+++ src/main/java/leetcode/array/twopointer/slidingwindow/Solution3.java
@@ -1,20 +1,71 @@
 package leetcode.array.twopointer.slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author enyi.lr
  * @version $Id: Solution3.java, v 0.1 2019‐05‐23 1:43 AM enyi.lr Exp $$
+ * v2
  */
 public class Solution3 {
 
     public static void main(String[] args) {
         Solution3 solution3 = new Solution3();
-        System.out.println(solution3.lengthOfLongestSubstring(""));
+        System.out.println(solution3.lengthOfLongestSubstring("abcabcbb"));
 
     }
 
     public int lengthOfLongestSubstring(String s) {
-        return lengthOfLongestSubstring1(s);
+        return lengthOfLongestSubstring3(s);
     }
+
+    // new method: use map and sliding window to solve the problem
+    public int lengthOfLongestSubstring3(String s) {
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = -1;
+        int longestLength = 0;
+        int length = chars.length;
+        Map<Character, Integer> frencuencyMap = new HashMap<>();
+        // use [l,r] to reserve the un repeated string
+        while (l < length && r + 1 < length) {
+            Integer index = frencuencyMap.get(chars[r + 1]);
+            if (index != null && index>=l) {
+                l = index;
+                l++;
+                r++;
+            } else {
+                r++;
+                longestLength = getMax(longestLength, r - l + 1);
+            }
+            frencuencyMap.put(chars[r], r);
+        }
+        return longestLength;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = -1;
+        int longestLength = 0;
+        int length = chars.length;
+        // use [l,r] to reserve the un repeated string
+        while (l < length && r + 1 < length) {
+            int index = containsIndex(chars, l, r, chars[r + 1]);
+            if (index == -1) {
+                r++;
+            } else {
+                l = index + 1;
+                r++;
+            }
+            longestLength = getMax(longestLength, r - l + 1);
+
+        }
+        return longestLength;
+    }
+
+
 
     public int lengthOfLongestSubstring1(String s) {
         char[] chars = s.toCharArray();
