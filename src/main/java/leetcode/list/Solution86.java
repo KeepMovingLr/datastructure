91935a50d16ab0cb5aac9863b53d448bd180b331
恩翊 <enyi.lr@cainiao.com>
2019-11-17 19:05:36 +0800

    solution86

--- src/main/java/leetcode/list/Solution86.java
+++ src/main/java/leetcode/list/Solution86.java
@@ -5,8 +5,42 @@ import leetcode.assiststructure.ListNode;
 /**
  * @author enyi.lr
  * @version $Id: Solution86.java, v 0.1 2019‐06‐15 1:43 AM enyi.lr Exp $$
+ * v2
  */
 public class Solution86 {
+
+    // todo
+    public ListNode partition2(ListNode head, int x) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        // variable: satisfiedPre  satisfied  pre  cur
+        ListNode satisfiedPre = dummyHead;
+        ListNode satisfied = head;
+        ListNode pre = dummyHead;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val < x) {
+                satisfied = satisfied.next;
+                satisfiedPre = satisfiedPre.next;
+            } else {
+                if (satisfied != cur) {
+                    pre.next = cur.next;
+                    satisfiedPre.next = cur;
+                    cur.next = satisfied;
+                    satisfiedPre = satisfiedPre.next;
+                }
+            }
+            cur = cur.next;
+            pre = pre.next;
+        }
+        return dummyHead.next;
+    }
+
+
     public ListNode partition(ListNode head, int x) {
         ListNode satisfieddummyHead = new ListNode(Integer.MAX_VALUE);
         ListNode satisfieddummyHeadNext = satisfieddummyHead;
