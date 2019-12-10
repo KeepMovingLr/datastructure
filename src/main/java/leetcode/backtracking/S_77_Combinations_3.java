066fea3b335c8a99bd3436f30afefa9a9f15224e
恩翊 <enyi.lr@cainiao.com>
2019-12-10 13:34:07 +0800

    combination problem

---
+++ src/main/java/leetcode/backtracking/S_77_Combinations_3.java
@@ -0,0 +1,64 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author enyi.lr
+ * @version $Id: S_77_Combinations.java, v 0.1 2019‐12‐07 8:14 PM enyi.lr Exp $$
+ */
+public class S_77_Combinations_3 {
+
+    public static void main(String[] args) {
+        S_77_Combinations_3 combinations = new S_77_Combinations_3();
+        System.out.println(combinations.combine(4, 3));
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        return combineList(list, k);
+    }
+
+    // 先比上一版解法，增加了减枝操作
+    public List<List<Integer>> combineList(List<Integer> list, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (k == 0) {
+            return result;
+        }
+        if (k > list.size()) {
+            return result;
+        }
+        if (k == list.size()) {
+            result.add(list);
+            return result;
+        }
+        if (k == 1) {
+            for (Integer num : list) {
+                List<Integer> oneRes = new ArrayList<>();
+                oneRes.add(num);
+                result.add(oneRes);
+            }
+            return result;
+        }
+        // list.size() >= k 减枝操作
+        while (!list.isEmpty() && list.size() >= k) {
+            int selected = list.remove(0);
+            int selectCount = k;
+            List<Integer> list2 = new ArrayList<>();
+            for (Integer num : list) {
+                list2.add(num);
+            }
+            selectCount--;
+            List<List<Integer>> lists = combineList(list2, selectCount);
+            for (List<Integer> res : lists) {
+                res.add(selected);
+            }
+            result.addAll(lists);
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/leetcode/backtracking/S_93_RestoreIPAddresses.java
@@ -0,0 +1,75 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author enyi.lr
+ * @version $Id: S_93_RestoreIPAddresses.java, v 0.1 2019‐12‐09 1:15 PM enyi.lr Exp $$
+ */
+public class S_93_RestoreIPAddresses {
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> stringResult = new ArrayList<>();
+        if (s.length() > 12) {
+            return stringResult;
+        }
+        List<List<String>> result = restoreIpAddressesHelper(s, 5);
+        for (List<String> list : result) {
+            Collections.reverse(list);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s1 : list) {
+                stringBuilder.append(s1).append(".");
+            }
+            stringResult.add(stringBuilder.toString().substring(0, stringBuilder.length() - 1));
+        }
+        return stringResult;
+    }
+
+    public List<List<String>> restoreIpAddressesHelper(String s, int count) {
+        List<List<String>> result = new ArrayList<>();
+        count--;
+        if (count == 1) {
+            if (!isValidIp(s)) {
+                return result;
+            } else {
+                List<String> oneAnswer = new ArrayList<>();
+                oneAnswer.add(s);
+                result.add(oneAnswer);
+                return result;
+            }
+        }
+        for (int i = 1; i < 4 && i < s.length(); i++) {
+            String lengthOne = s.substring(0, i);
+            if (isValidIp(lengthOne)) {
+                List<List<String>> oneResult = restoreIpAddressesHelper(s.substring(i), count);
+                for (List<String> list : oneResult) {
+                    list.add(lengthOne);
+                }
+                result.addAll(oneResult);
+            }
+        }
+        return result;
+    }
+
+    // each part of a valid ip is between [0,255]
+    public boolean isValidIp(String needCheckedIp) {
+        if (needCheckedIp.length() != 1) {
+            if (needCheckedIp.startsWith("0")) {
+                return false;
+            }
+        }
+        long ip = Long.parseLong(needCheckedIp);
+        return ip >= 0 && ip <= 255;
+    }
+
+    public static void main(String[] args) {
+        S_93_RestoreIPAddresses s_93_restoreIPAddresses = new S_93_RestoreIPAddresses();
+        List<String> list = s_93_restoreIPAddresses.restoreIpAddresses("0279245587303");
+        //List<String> list2 = s_93_restoreIPAddresses.restoreIpAddresses("0000");
+        System.out.println(list);
+
+    }
+
+}
