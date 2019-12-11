bafa13904db7a5810c1ddb9fca05b95d9429e595
恩翊 <enyi.lr@cainiao.com>
2019-12-11 14:25:05 +0800

    combination sum optimize

---
+++ src/main/java/leetcode/backtracking/S_216_CombinationSumIII.java
@@ -0,0 +1,61 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author enyi.lr
+ * @version $Id: S_216_CombinationSumIII.java, v 0.1 2019‐12‐11 12:57 PM enyi.lr Exp $$
+ */
+public class S_216_CombinationSumIII {
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (k > 9) {
+            return result;
+        }
+        if (n < 0 && n > 45) {
+            return result;
+        }
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<List<Integer>> lists = combinationSum3(nums, k, n, 0);
+        for (List<Integer> list : lists) {
+            if (list.size() == k) {
+                result.add(list);
+            }
+        }
+        return result;
+    }
+
+    public List<List<Integer>> combinationSum3(int[] nums, int k, int target, int start) {
+        List<List<Integer>> result = new ArrayList<>();
+        // recursive termination condition
+        if (k == 0) {
+            return result;
+        }
+        if (start >= nums.length) {
+            return result;
+        }
+        if (nums[start] > target) {
+            return result;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            int selected = nums[i];
+            if (selected == target) {
+                List<Integer> oneAnswer = new ArrayList<>();
+                oneAnswer.add(selected);
+                result.add(oneAnswer);
+                // as the number is sorted, so we can return now
+                return result;
+            }
+            List<List<Integer>> lists = combinationSum3(nums, k - 1, target - selected, i + 1);
+            for (List<Integer> list : lists) {
+                list.add(selected);
+            }
+            result.addAll(lists);
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/leetcode/backtracking/S_39_CombinationSum_3.java
@@ -0,0 +1,55 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author enyi.lr
+ * @version $Id: S_39_CombinationSum.java, v 0.1 2019‐12‐08 3:04 PM enyi.lr Exp $$
+ */
+public class S_39_CombinationSum_3 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return result;
+        }
+        Arrays.sort(candidates);
+        // recursive termination condition is candidates[0] > target, as all numbers are positive integers.
+        boolean hasAnswer = candidates[0] <= target;
+        if (!hasAnswer) {
+            return result;
+        }
+        for (int i = 0; i < candidates.length; i++) {
+            int selected = candidates[i];
+            if (selected > target) {
+                return result;
+            }
+            if (target == selected) {
+                List<Integer> oneAnswer = new ArrayList<>();
+                oneAnswer.add(target);
+                result.add(oneAnswer);
+                // as the array is sorted, so this case, we can return immediately
+                return result;
+            } else {
+                // candidates[0] <= target-selected 减枝操作
+                if (candidates[0] <= target-selected){
+                    List<List<Integer>> lists = combinationSum(candidates, target - selected);
+                    for (List<Integer> list : lists) {
+                        // 这里由于需要去除，是否可以优化？
+                        list.add(selected);
+                        Collections.sort(list);
+                        if (!result.contains(list)) {
+                            result.add(list);
+                        }
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+
+}
