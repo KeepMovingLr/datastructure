07e5628a86ccef2607c254e948cf91932a2cb18f
Ray <1040375843@qq.com>
2023-09-04 08:43:37 +0800

    add union find

--- src/main/java/unionFind/UF.java
+++ src/main/java/unionFind/UF.java
@@ -3,6 +3,11 @@ package unionFind;
 /**
  * @author enyi.lr
  * @version $Id: UF.java, v 0.1 2019‐06‐02 5:29 PM enyi.lr Exp $$
+ * Union - Find
+ * Find:
+ * The find function finds the root node of a given vertex.
+ * Union:
+ * The union function unions two vertices and makes their root nodes the same.
  */
 public interface UF {
     int getSize();
