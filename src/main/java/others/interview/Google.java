754ee89d0bd92b1f42b91cf390f60d563b63a57e
ray.li <ray.li@shopee.com>
2024-01-26 13:34:17 +0800

    questions

--- src/main/java/others/interview/Google.java
+++ src/main/java/others/interview/Google.java
@@ -28,10 +28,8 @@ import java.util.Arrays;
  * favourite = []
  * 调用getPhoto 3次后的输出为: -1 -1 -1
  * <p>
- * 作者：长城上跑步
+ *
  * 链接：https://leetcode.cn/circle/discuss/YqC4Gz/
- * 来源：力扣（LeetCode）
- * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class Google {
 
--- src/main/java/others/interview/Google2.java
+++ src/main/java/others/interview/Google2.java
@@ -3,14 +3,12 @@ package others.interview;
 /**
  * 有几个学生被淘汰了？
  * 给你一个二维数组，a[i][0]代表第i个学生的math成绩，a[i][1]代表第i个学生的English成绩。 如果存在学生b的两门成绩都比学生a高，a就被淘汰了。问一共有几个学生被淘汰了？
+ * (1, 2)
+ * (3, 4)
+ * (5, 6)
+ * (7, 8)
  *
- *
- *
- *
- * 作者：长城上跑步
- * 链接：https://leetcode.cn/circle/discuss/YqC4Gz/
- * 来源：力扣（LeetCode）
- * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * https://leetcode.cn/circle/discuss/YqC4Gz/
  */
 public class Google2 {
 
