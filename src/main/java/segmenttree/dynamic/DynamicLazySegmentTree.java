07d47a7417274c0cad525b8f9a88cccb48004c39
ray.li <ray.li@shopee.com>
2024-02-13 16:34:06 +0800

    My dynamic lazy tree with range update

--- src/main/java/segmenttree/dynamic/DynamicLazySegmentTree.java
+++ src/main/java/segmenttree/dynamic/DynamicLazySegmentTree.java
@@ -31,7 +31,7 @@ public class DynamicLazySegmentTree {
         }
     }
 
-    static SegmentNode root = new SegmentNode(0, 100);
+    static SegmentNode root = new SegmentNode(0, 10);
 //    static SegmentNode root = new SegmentNode(0, (int) 1e9);
 
     public static void update(int l, int r, int value) {
@@ -47,6 +47,8 @@ public class DynamicLazySegmentTree {
             cur.lazyRight += lazyValue;
             return;
         }
+        if (l > cur.rangeR || r < cur.rangeL)
+            return;
 
         if (l <= cur.rangeL && r >= cur.rangeR) {
             cur.lazyLeft += value;
@@ -54,25 +56,78 @@ public class DynamicLazySegmentTree {
             cur.val += (cur.rangeR - cur.rangeL + 1) * value;
             return;
         }
-        if (l < cur.rangeM) {
+        if (l <= cur.rangeM) {
             if (cur.leftChild == null)
                 cur.leftChild = new SegmentNode(cur.rangeL, cur.rangeM);
             cur.leftChild.lazyVal = cur.lazyLeft;
             cur.lazyLeft = 0;
             update(cur.leftChild, l, r, value);
         }
-        if (r > cur.rangeM) {
+        if (r >= cur.rangeM + 1) {
             if (cur.rightChild == null)
-                cur.rightChild = new SegmentNode(cur.rangeM, cur.rangeR);
+                cur.rightChild = new SegmentNode(cur.rangeM + 1, cur.rangeR);
             cur.rightChild.lazyVal = cur.lazyRight;
             cur.lazyRight = 0;
             update(cur.rightChild, l, r, value);
         }
     }
 
+    public static int query(int l, int r) {
+        if (l > root.rangeR || r < root.rangeL)
+            return 0;
+        return query(root, l, r);
+    }
+
+    public static int query(SegmentNode cur, int l, int r) {
+        int lazyValue = cur.lazyVal;
+        if (lazyValue != 0) {
+            cur.val += (cur.rangeR - cur.rangeL + 1) * lazyValue;
+            cur.lazyVal = 0;
+            cur.lazyLeft += lazyValue;
+            cur.lazyRight += lazyValue;
+        }
+        if (l <= cur.rangeL && r >= cur.rangeR) {
+            return cur.val;
+        }
+        if (l > cur.rangeR || r < cur.rangeL)
+            return 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        if (r <= cur.rangeM) {
+            if (cur.leftChild == null) {
+                cur.leftChild = new SegmentNode(cur.rangeL, cur.rangeM);
+                cur.leftChild.lazyVal = cur.lazyLeft;
+                cur.lazyLeft = 0;
+            }
+            return query(cur.leftChild, l, r);
+        }
+        if (l >= cur.rangeM + 1) {
+            if (cur.rightChild == null) {
+                cur.rightChild = new SegmentNode(cur.rangeM + 1, cur.rangeR);
+                cur.rightChild.lazyVal = cur.lazyRight;
+                cur.lazyRight = 0;
+            }
+            return query(cur.rightChild, l, r);
+        }
+        if (cur.leftChild == null) {
+            cur.leftChild = new SegmentNode(cur.rangeL, cur.rangeM);
+            cur.leftChild.lazyVal = cur.lazyLeft;
+            cur.lazyLeft = 0;
+        }
+        leftSum = query(cur.leftChild, l, r);
+        if (cur.rightChild == null) {
+            cur.rightChild = new SegmentNode(cur.rangeM + 1, cur.rangeR);
+            cur.rightChild.lazyVal = cur.lazyRight;
+            cur.lazyRight = 0;
+        }
+        rightSum = query(cur.rightChild, l, r);
+        return leftSum + rightSum;
+    }
+
     public static void main(String[] args) {
-        update( 40, 60 ,10);
         System.out.println();
+        update(40, 60, 10);
+        System.out.println(query(0, 100));
     }
 
 }
