1d8069ec483f67de43adc5def1fe4a5eaad05714
Ray <1040375843@qq.com>
2024-02-14 23:03:55 +0800

    trie recursive method

--- src/main/java/trie/Trie.java
+++ src/main/java/trie/Trie.java
@@ -5,9 +5,10 @@ import java.util.TreeMap;
 /**
  * @author enyi.lr
  * @version $Id: Trie.java, v 0.1 2019‐06‐01 7:18 PM enyi.lr Exp $$
- *  pronouncing it /ˈtriː/ (as "tree"), after the middle syllable of retrieval. However, other authors pronounce it /ˈtraɪ/ (as "try")
- *  Trie - Prefix tree
- *  A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings.
+ * pronouncing it /ˈtriː/ (as "tree"), after the middle syllable of retrieval. However, other authors pronounce it /ˈtraɪ/ (as "try")
+ * Trie - Prefix tree
+ * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings.
+ * https://en.wikipedia.org/wiki/Trie#:~:text=In%20computer%20science%2C%20a%20trie,key%2C%20but%20by%20individual%20characters.
  */
 public class Trie {
     private class Node {
@@ -26,7 +27,7 @@ public class Trie {
     }
 
     private Node root;
-    private int  size;
+    private int size;
 
     public Trie() {
         root = new Node();
@@ -54,6 +55,7 @@ public class Trie {
         }
         if (current.isWord == false) {
             current.isWord = true;
+            size++;
         }
 
     }
@@ -75,6 +77,20 @@ public class Trie {
 
     }
 
+    public boolean containsRec(String word) {
+        return _containsRec(root, 0, word);
+    }
+
+    private boolean _containsRec(Node root, int fromIdx, String word) {
+        if (fromIdx == word.length()) return root.isWord;
+        char c = word.charAt(fromIdx);
+        if (!root.next.containsKey(c)) {
+            return false;
+        }
+        Node node = root.next.get(c);
+        return _containsRec(node, fromIdx + 1, word);
+    }
+
     /**
      * check if the trie contains the word which prefex is param prefix
      *
@@ -92,21 +108,45 @@ public class Trie {
         return true;
     }
 
+    public boolean isPrefixRec(String prefix) {
+        return isPrefixRec(root, 0, prefix);
+    }
+
+    private boolean isPrefixRec(Node root, int fromIdx, String prefix) {
+        if (fromIdx == prefix.length())
+            return true;
+        char c = prefix.charAt(fromIdx);
+        if (!root.next.containsKey(c))
+            return false;
+        Node node = root.next.get(c);
+        return isPrefixRec(node, fromIdx + 1, prefix);
+    }
+
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.add("hello");
         trie.add("world");
         System.out.println(trie.contains("hell"));
-        System.out.println(trie.isPrefix("hell"));
+        System.out.println(trie.containsRec("hell"));
         System.out.println(trie.contains("hello"));
-        System.out.println(trie.isPrefix("hello"));
+        System.out.println(trie.containsRec("hello"));
         System.out.println(trie.contains("wor"));
-        System.out.println(trie.isPrefix("wor"));
+        System.out.println(trie.containsRec("wor"));
         System.out.println(trie.contains("world"));
-        System.out.println(trie.isPrefix("world"));
-        System.out.println(trie.isPrefix("m"));
+        System.out.println(trie.containsRec("world"));
         System.out.println(trie.contains("m"));
+        System.out.println(trie.containsRec("m"));
         System.out.println(trie.getSize());
+        System.out.println(trie.isPrefix("hell"));
+        System.out.println(trie.isPrefixRec("hell"));
+        System.out.println(trie.isPrefix("hello"));
+        System.out.println(trie.isPrefixRec("hello"));
+        System.out.println(trie.isPrefix("wor"));
+        System.out.println(trie.isPrefixRec("wor"));
+        System.out.println(trie.isPrefix("world"));
+        System.out.println(trie.isPrefixRec("world"));
+        System.out.println(trie.isPrefix("m"));
+        System.out.println(trie.isPrefixRec("m"));
 
     }
 
