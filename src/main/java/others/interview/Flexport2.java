d45394a3b9f24b1f5900e7f2df1b5ca917dd030c
ray.li <ray.li@shopee.com>
2024-05-03 18:59:03 +0800

    solved one

--- src/main/java/others/interview/Flexport2.java
+++ src/main/java/others/interview/Flexport2.java
@@ -1,5 +1,7 @@
 package others.interview;
 
+import java.util.*;
+
 /**
  * Generate a random sentence of length N from a given text. Logic:
  * 1. Randomly select a word
@@ -16,4 +18,61 @@ package others.interview;
  * Outout: "flexport interview and this"
  */
 public class Flexport2 {
+    String[] words;
+    Map<String , List<Integer>> wordIdx = new HashMap<>();
+    int idx;
+    public String randomSelect(int n , String sentence) {
+        words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if(!wordIdx.containsKey(words[i])) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                wordIdx.put(words[i] , list);
+            } else {
+                List<Integer> list = wordIdx.get(words[i]);
+                list.add(i);
+                wordIdx.put(words[i] , list);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        // get the first random
+        idx = getRandoNum(words.length);
+        sb.append(words[idx]);
+        sb.append(" ");
+        if(n == 1)
+            return sb.toString();
+        for(int i = 1 ; i < n ; i++) {
+            if(idx == words.length - 1) {
+                idx = 0;
+            } else {
+                idx = idx + 1;
+            }
+            sb.append(select());
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    private int getRandoNum(int num) {
+        Random random = new Random();
+        return random.nextInt(num);
+    }
+
+    public String select() {
+        String word = words[idx];
+        List<Integer> list = wordIdx.get(word);
+        if(list.size() == 1) {
+            return word;
+        } else {
+            int listIdx = getRandoNum(list.size());
+            idx = list.get(listIdx);
+            return word;
+        }
+    }
+
+    public static void main(String[] args) {
+        Flexport2 flexport = new Flexport2();
+        String s = flexport.randomSelect(4, "hello this is a flexport interview and this is a hello and that was cool");
+        System.out.println(s);
+    }
 }
