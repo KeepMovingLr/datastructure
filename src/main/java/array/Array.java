1ef468b064652df5c9f2a3b8c176f9c01fe113e3
Ray <1040375843@qq.com>
2024-08-26 16:43:54 -0500

    dynamic array

--- src/main/java/array/Array.java
+++ src/main/java/array/Array.java
@@ -3,7 +3,9 @@ package array;
 /**
  * @author enyi.lr
  * @version $Id: Array.java, v 0.1 2019‐05‐15 12:02 PM enyi.lr Exp $$
- * Dynamic array
+ * Dynamic array.
+ * A Dynamic array (vector in C++, ArrayList in Java) automatically grows when we try to make an insertion
+ * and there is no more space left for the new item.
  */
 public class Array<E> {
     private E[] data;
